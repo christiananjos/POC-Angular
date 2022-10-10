cc5f85edc472211281d89295af1ba9afc493a89e
Christian Douglas dos Anjos <christian.anjos-sys@parceirosedu.com.br>
2022-10-10 14:31:58 -0300

    Adicionado Tables para gerenciamento

--- pocAppAngular/src/app/avaliacao/avaliacao.component.ts
+++ pocAppAngular/src/app/avaliacao/avaliacao.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface avaliacoes {
+  nomeCliente: string;
+  dieta: string;
+  data: string;
+  detalhes: string;
+}
+
+const ELEMENT_DATA: avaliacoes[] = [
+  { nomeCliente: 'Cliente 1', dieta: 'Exemplo plano 1', data: '10/10/2022', detalhes: '' },
+  { nomeCliente: 'Cliente 2', dieta: 'Exemplo plano 1', data: '10/10/2022', detalhes: '' },
+  { nomeCliente: 'Cliente 3', dieta: 'Exemplo plano 1', data: '10/10/2022', detalhes: '' },
+  { nomeCliente: 'Cliente 4', dieta: 'Exemplo plano 1', data: '10/10/2022', detalhes: '' },
+  
+];
+
 
 @Component({
   selector: 'app-avaliacao',
   templateUrl: './avaliacao.component.html',
   styleUrls: ['./avaliacao.component.css']
 })
-export class AvaliacaoComponent implements OnInit {
+export class AvaliacaoComponent {
+  displayedColumns: string[] = ['nomeCliente', 'dieta', 'data', 'detalhes'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
 
-  constructor() { }
-
-  ngOnInit(): void {
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
--- pocAppAngular/src/app/dieta/dieta.component.ts
+++ pocAppAngular/src/app/dieta/dieta.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface dieta {
+  nomeCliente: string;
+  dieta: string;
+  data: string;
+  detalhes: string;
+}
+
+const ELEMENT_DATA: dieta[] = [
+  { nomeCliente: 'Cliente 1', dieta: 'Exemplo plano 1', data: '10/10/2022', detalhes: '' },
+  { nomeCliente: 'Cliente 2', dieta: 'Exemplo plano 1', data: '10/10/2022', detalhes: '' },
+  { nomeCliente: 'Cliente 3', dieta: 'Exemplo plano 1', data: '10/10/2022', detalhes: '' },
+  { nomeCliente: 'Cliente 4', dieta: 'Exemplo plano 1', data: '10/10/2022', detalhes: '' },
+
+];
+
 
 @Component({
   selector: 'app-dieta',
   templateUrl: './dieta.component.html',
   styleUrls: ['./dieta.component.css']
 })
-export class DietaComponent implements OnInit {
 
-  constructor() { }
+export class DietaComponent {
+  displayedColumns: string[] = ['nomeCliente', 'dieta', 'data', 'detalhes'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
 
-  ngOnInit(): void {
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
